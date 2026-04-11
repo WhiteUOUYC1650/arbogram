9490add23ea63260e3fbec5d17032752357f50c8
Фёдор Сорокин <sorokinfedor715@gmail.com>
2026-04-11 09:57:48 +0000

    Самолётик сжался, выглядит неприятно

--- src/components/arbogram-icon.tsx
+++ src/components/arbogram-icon.tsx
@@ -1,4 +1,6 @@
 
+"use client";
+
 import { cn } from "@/lib/utils";
 
 interface ArbogramIconProps {
@@ -9,31 +11,32 @@ export function ArbogramIcon({ className }: ArbogramIconProps) {
   return (
     <svg 
       viewBox="0 0 100 100" 
-      className={cn("overflow-hidden rounded-2xl shadow-lg", className)}
+      className={cn("overflow-hidden rounded-2xl shadow-lg aspect-square shrink-0", className)}
       xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
     >
-      {/* Background Stripes */}
+      {/* Background Stripes (Yellow, Red, Blue) */}
       <rect width="100" height="33.3" fill="#FFED00" />
       <rect y="33.3" width="100" height="33.4" fill="#DA121A" />
       <rect y="66.7" width="100" height="33.3" fill="#0051A5" />
       
-      {/* Paper Plane Overlay */}
-      <path 
-        d="M20 45 L80 15 L50 85 L45 55 L20 45 Z" 
-        fill="white" 
-        stroke="black" 
-        strokeWidth="4" 
-        strokeLinejoin="round" 
-        transform="translate(0, 5) scale(0.9) rotate(-10, 50, 50)"
-      />
-      <path 
-        d="M45 55 L80 15" 
-        fill="none" 
-        stroke="black" 
-        strokeWidth="3" 
-        strokeLinecap="round"
-        transform="translate(0, 5) scale(0.9) rotate(-10, 50, 50)"
-      />
+      {/* Paper Plane Overlay - centered and properly scaled */}
+      <g transform="translate(50, 50) rotate(-10)">
+        <path 
+          d="M-30,-5 L30,-35 L0,35 L-5,5 L-30,-5 Z" 
+          fill="white" 
+          stroke="black" 
+          strokeWidth="4" 
+          strokeLinejoin="round" 
+        />
+        <path 
+          d="M-5,5 L30,-35" 
+          fill="none" 
+          stroke="black" 
+          strokeWidth="3" 
+          strokeLinecap="round"
+        />
+      </g>
     </svg>
   );
 }
