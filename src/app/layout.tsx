671fd2b96fb473deb46c011968f8e2e80a4727f8
Фёдор Сорокин <sorokinfedor715@gmail.com>
2026-04-11 10:13:50 +0000

    На этот раз НЗССП не надо, подготовь приложение к сборке в апк через git

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'Arbogram',
   description: 'Настоящий мессенджер на Firebase',
+  manifest: '/manifest.json',
   appleWebApp: {
     capable: true,
     statusBarStyle: 'default',
@@ -37,6 +38,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
       <body className="font-body antialiased overflow-hidden">
         <FirebaseClientProvider>
